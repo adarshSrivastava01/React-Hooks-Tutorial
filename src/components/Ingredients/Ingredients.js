ee33dddac3222f35d136f9dae8c1c36e1d445e6d
Adarsh Srivastava <herecomesthebeastlesnar@gmail.com>
2020-11-05 16:26:00 +0530

    Before useEffect

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    fetch("https://react-hooks-9ec10.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      });
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
